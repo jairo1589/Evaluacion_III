c46aecd71962ac942f53dc74080bd4be16ea8e47
jairo <jairo15895@gmail.com>
2020-11-27 17:07:53 -0300

    se agrego el video

--- app/src/main/java/com/example/pizzeria/Menu_act.java
+++ app/src/main/java/com/example/pizzeria/Menu_act.java
@@ -3,17 +3,35 @@ package com.example.pizzeria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import java.util.ArrayList;
 
 public class Menu_act extends AppCompatActivity {
 
+    private VideoView videoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_act);
+
+        videoView = (VideoView)findViewById(R.id.videov);
+
+
+        String ruta = "android.resource://" + getPackageName() + "/" + R.raw.video;
+
+        Uri uri = Uri.parse(ruta);
+        videoView. setVideoURI(uri);
+
+
+        MediaController media = new MediaController(this);
+        videoView.setMediaController(media);
+
     }
 
     public void promociones (View v)
